a1c37f41721677f182ce04779ef64c92d136bc79
Jørgen Hookham <JorgenHookham@Gmail.com>
2013-03-02 23:37:50 -0800

    Create javascript unit test skeleton.

---
+++ tests/unit-tests.js
@@ -0,0 +1,13 @@
+define([
+
+    'jquery',
+    'underscore',
+    'backbone'
+
+], function ($, _, Backbone) {
+
+    test('Universal truth test.', function () {
+        ok(1 == 1);
+    });
+
+});
