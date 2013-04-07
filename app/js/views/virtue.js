0c076df4aaf4176e5fb0426cd4c94edfb6948985
Jørgen Hookham <JorgenHookham@Gmail.com>
2013-04-06 21:30:46 -0700

    Update nomenclature and create virtues view.

--- app/js/views/virtue.js
+++ app/js/views/virtue.js
@@ -8,29 +8,30 @@ define([
 ], function ($, _, Backbone, VirtueTemplate) {
     
     /*
-     * This is how Jiminy shows you one of your virtues.
+     * This is how Jiminy shows you one of your virtues and lets you
+     * track moments that you want to reflect on.
      */
 
     var VirtueView = Backbone.View.extend({
 
-        events: {
-            'click [data-increment]' : 'add_relection_tally'
+        'events': {
+            'click [data-increment]' : 'ui--create-tally'
         },
 
-        className: 'tally--virtue',
+        'className': 'tally--virtue',
 
-        initialize: function (settings) {
+        'initialize': function (settings) {
             if (settings && settings.model) {
                 this.model = settings.model;
             }
         },
 
-        render: function () {
+        'render': function () {
             _.template(VirtueTemplate, {virtue: this.model});
             return this;
         },
 
-        add_relection_tally: function (e) {
+        'ui--create-tally': function (e) {
             e.preventDefault();
             Backbone.Mediator.pub('reflection:create-tally', this.model);
         }
---
+++ app/js/views/virtues.js
@@ -0,0 +1,17 @@
+define([
+
+    'jquery',
+    'underscore',
+    'backbone'
+
+], function ($, _, Backbone) {
+
+    /*
+     * This is your list of virtues.
+     */
+
+    var VirtuesView = Backbone.View.extend({});
+
+    return VirtuesView;
+
+});
